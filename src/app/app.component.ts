04013a87e134ae227fe5407145dee51f7dcc9fd1
Marty <marty@ilikeemail.com>
2020-10-05 20:29:06 -0400

    Tweaks the title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MAP-OF-ANGULAR';
+export class AppComponent {  
+  title = 'Marty The Party\'s MAP OF ANGULAR';
 }
